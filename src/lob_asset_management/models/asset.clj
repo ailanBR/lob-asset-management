ae3bb49101dbc81f32ec29ca41b7a34a134d3237
ailan <ailancerbaa@gmail.com>
2023-05-16 22:16:07 -0300

    add-group-code-location-in-release

--- src/lob_asset_management/models/asset.clj
+++ src/lob_asset_management/models/asset.clj
@@ -2,8 +2,9 @@
   (:require [schema.core :as s]))
 
 (def crypto-list #{:BTC :ETH :CAKE :BNB :ALGO :LUNA :FANTOM})
-
 (def fii-list #{:ALZR11 :HGBS11 :KNRI11 :RECR11})
+(def etf-list #{:DEFI11})
+(def bdr-list #{:NUBR33 :INBR31})
 
 (s/defschema Asset
   {:asset/id            s/Uuid
